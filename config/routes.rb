7b96f895e30e87410c477d20dedd5efbe2d080b8
Shubham Mongia <shubham@veersatech.com>
2021-08-18 17:29:38 +0530

    Added new and edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'about', to: 'about#page'
-  resources :articles , only: [:new, :show, :index, :create]
+  resources :articles , only: [:new, :show, :index, :create, :edit, :update]
 end
