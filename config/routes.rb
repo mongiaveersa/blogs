36cf75bfd48f10ba956417cb2e282828031db7fe
Shubham Mongia <shubham@veersatech.com>
2021-08-18 15:14:01 +0530

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'about', to: 'about#page'
 end
