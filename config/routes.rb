fe93934baab52d238ec9d0683c8de3347af3d96a
Shubham Mongia <shubham@veersatech.com>
2021-08-18 16:42:19 +0530

    Added show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'about/page'
+  # get 'about/page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'about', to: 'about#page'
+  resources :articles , only: [:show]
 end
