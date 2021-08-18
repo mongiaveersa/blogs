d6746831556a2902cbde3720a9579cad55395c2f
Shubham Mongia <shubham@veersatech.com>
2021-08-18 21:45:45 +0530

    Refactoring code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def show 
-        @article=Article.find(params[:id])
+        
     end
 
     def index
@@ -11,11 +12,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article=Article.find(params[:id])
+        
     end
 
     def create
-        @article= Article.new(params.require(:article).permit(:title, :description))
+        @article= Article.new(article_params)
         # render plain: @article.inspect
         if @article.save 
             flash[:notice]="Article successfully created "
@@ -27,8 +28,8 @@ class ArticlesController < ApplicationController
 
 
     def update 
-        @article=Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        
+        if @article.update(article_params)
             flash[:notice]="Article successfully updated "
             redirect_to article_path(@article)
         else
@@ -36,7 +37,20 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def destroy 
+        
+        @article.destroy
+        redirect_to articles_path
+    end
+    
+    
+    private
 
-
+    def set_article
+        @article= Article.find(params[:id])
+    end
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'about', to: 'about#page'
-  resources :articles , only: [:new, :show, :index, :create, :edit, :update]
+  resources :articles 
+  # , only: [:new, :show, :index, :create, :edit, :update, :destroy]
 end
