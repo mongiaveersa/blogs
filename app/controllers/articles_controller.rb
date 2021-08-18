dc82b4b3e1f243924c92fe23b945cdeca3071fba
Shubham Mongia <shubham@veersatech.com>
2021-08-18 17:01:24 +0530

    Added new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,15 @@ class ArticlesController < ApplicationController
         @article=Article.find(params[:id])
     end
 
+    def index
+        @articles=Article.all
+    end
+    def new
+    end
+
+    def create
+        render plain: params[:article]
+    end
+
+
 end
